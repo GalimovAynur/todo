9e0ee87e4eef4984e702d83fa5193d5f5890cca9
Galimov Aynur <aynuuu@gmail.com>
2017-11-10 00:35:47 +0500

    Add edit, update view and controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,12 +11,26 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
     @task = Task.new(task_params)
     if @task.save
       redirect_to @task
     else
       render :new
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :edit
     end
   end
 
