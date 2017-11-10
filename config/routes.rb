c9bea3dac7fc49ad5bf214887f0a1f83abe206ad
Galimov Aynur <aynuuu@gmail.com>
2017-11-10 17:07:26 +0500

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
-  
+
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
